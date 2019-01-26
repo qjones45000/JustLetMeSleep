c69d170811bc6ec5a1b341382a8cfe46aa9e2802
Tranzorro <shiekadam@gmail.com>
2019-01-26 03:26:41 -0500

    particle system script update
    added one

---
+++ JustLetMeSleep/Assets/UI/WeatherTimerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherTimerScript : MonoBehaviour
+{
+    //insert rain effect here
+    public ParticleSystem rain;
+    int rainChance = 2;
+    int pickrand;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        InvokeRepeating("pickWeather", 5, 20);
+    }
+    void PickWeather()
+    {
+         pickrand = Random.Range(0, 10);
+        if (pickrand == rainChance)
+        {
+            // put rain effect activation here
+            rain.enableEmission = true;
+        }
+    }
+}
