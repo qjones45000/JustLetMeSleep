6e55e96d6a5fd85de3317f530187a3a49f5a960c
Tranzorro <shiekadam@gmail.com>
2019-01-26 02:33:44 -0500

    initial commit. contains ui and scripts
    plug scripts into canvas event system. drag object references to make it work

---
+++ JustLetMeSleep/Assets/UI/CatScoreScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatScoreScript : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText.text = "Cats: " + score;
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score = newScoreValue;
+        UpdateScore();
+    }
+}
